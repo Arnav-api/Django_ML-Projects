c5a038bc7aab38b96b60e7983712bf1ad1604a42
Arnav_Khandelwal_ML <aarnavlko@gmail.com>
2025-12-28 14:06:02 +0530

    Update settings.py

--- Ecommerce_Website/Neural_Networks/settings.py
+++ Ecommerce_Website/Neural_Networks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-t@379mq+u)q1fhp3em&9(w9^5je_m**clu0$0=g4vf!@zcg_on
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-ml-projects-6.onrender.com","django-ml-projects-7.onrender.com","django-ml-projects-8.onrender.com"]
+ALLOWED_HOSTS = ["django-ml-projects-6.onrender.com","django-ml-projects-7.onrender.com","django-ml-projects-8.onrender.com","django-ml-projects-9.onrender.com","django-ml-projects-10.onrender.com"]
 
 
 # Application definition
